696afade6b6f0affd2e8f5ad23afa98b546fb042
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-06-01 20:46:29 +0900

    update

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/ponkichi0111/oreilly-terraform-modules//services/webserver-cluster?ref=v0.0.2"
 
   cluster_name           = "webservices-stage"
   db_remote_state_bucket = "terraform-state-hiroyuki-2025-05-31"
