2682dd23c56b5d8be5bd74e3085ef47370e8601d
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-06-02 23:47:39 +0900

    upper for

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -16,10 +16,14 @@ provider "aws" {
 #   value = [for name in var.user_names : upper(name)]
 # }
 
-output "short_upper_names" {
-  value = [for name in var.user_names : upper(name) if length(name) < 5]
-}
+# output "short_upper_names" {
+#   value = [for name in var.user_names : upper(name) if length(name) < 5]
+# }
 
 output "bios" {
   value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
 }
