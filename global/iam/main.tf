b257a12e1e1b5062296f5f1d44bdb2d1c825a79b
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-06-02 22:57:34 +0900

    map

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -12,6 +12,14 @@ provider "aws" {
 #   value = values(aws_iam_user.example)[*].arn
 # }
 
-output "upper_names" {
-  value = [for name in var.user_names : upper(name)]
+# output "upper_names" {
+#   value = [for name in var.user_names : upper(name)]
+# }
+
+output "short_upper_names" {
+  value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
 }
--- global/iam/variables.tf
+++ global/iam/variables.tf
@@ -3,3 +3,13 @@ variable "user_names" {
   type = list(string)
   default = [ "neo", "trinity", "morpheus" ]
 }
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type = map(string)
+  default = {
+    "neo"      = "hero"
+    "trinity"  = "love interest"
+    "morpheus" = "mentor"
+  }
+}
