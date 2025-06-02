02b8b30a32394c22539962f036cd3a949bee7971
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-06-02 22:40:20 +0900

    for

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -3,7 +3,15 @@ provider "aws" {
   profile = "dev"
 }
 
-resource "aws_iam_user" "example" {
-  count = length(var.user_names)
-  name = var.user_names[count.index]
+# resource "aws_iam_user" "example" {
+#   for_each = toset(var.user_names)
+#   name     = each.value
+# }
+
+# output "all_users" {
+#   value = values(aws_iam_user.example)[*].arn
+# }
+
+output "upper_names" {
+  value = [for name in var.user_names : upper(name)]
 }
