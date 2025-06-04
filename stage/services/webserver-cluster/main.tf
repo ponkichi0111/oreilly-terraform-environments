5dbe6a9a92dacf98e44411b27da196c504453151
ponkichi0111 <z.hiroyuki.0111@gmail.com>
2025-06-04 23:31:17 +0900

    update

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -6,6 +6,9 @@ provider "aws" {
 module "webserver-cluster" {
   source = "../../../../modules/services/webserver-cluster"
 
+  ami = "ami-027fff96cc515f7bc"
+  server_text = "New server text"
+
   cluster_name           = "webservices-stage"
   db_remote_state_bucket = "terraform-state-hiroyuki-2025-05-31"
   db_remote_stae_key     = "stage/data-stores/mysql/terraform.tfstate"
